682d01b3b35377788425e163a2d6e0d3897edb3a
moxnix <andrewchall92@gmail.com>
2023-10-28 00:08:12 -0700

    Added the schema with database and tables

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS registry_db;
+CREATE DATABASE registry_db;
+
+USE registry_db;
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+DROP TABLE IF EXISTS role;
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salery DECIMAL,
+    department_id INT NOT NULL
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT 
+);
